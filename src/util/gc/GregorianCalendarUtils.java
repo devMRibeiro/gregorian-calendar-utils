64324f61a918a3428b37d34a715587a9c52eb2d8
devMRibeiro <michael.douglas2020@hotmail.com>
2025-03-13 08:23:04 -0300

    Method to get first and last day of month

---
+++ src/util/gc/GregorianCalendarUtils.java
@@ -0,0 +1,23 @@
+package util.gc;
+
+import java.util.GregorianCalendar;
+
+public class GregorianCalendarUtils {
+	public static GregorianCalendar resetTime(GregorianCalendar gc) {
+		gc.set(GregorianCalendar.HOUR_OF_DAY, 0);
+		gc.set(GregorianCalendar.MINUTE, 0);
+		gc.set(GregorianCalendar.SECOND, 0);
+		gc.set(GregorianCalendar.MILLISECOND, 0);
+		return gc;
+	}
+
+	public static GregorianCalendar getFirstDayOfMonth(GregorianCalendar gc) {
+		gc.set(GregorianCalendar.DAY_OF_MONTH, 1);
+		return resetTime(gc);
+	}
+
+	public static GregorianCalendar getLastDayOfMonth(GregorianCalendar gc) {
+		gc.set(GregorianCalendar.DAY_OF_MONTH, gc.getActualMaximum(GregorianCalendar.DAY_OF_MONTH));
+		return resetTime(gc);
+	}
+}
